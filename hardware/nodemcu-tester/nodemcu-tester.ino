0b4456dfb2d1e2dbe43258e714bced6526d5c5de
khaifahmi99 <khaifahmi99@github.com>
2020-05-14 15:00:05 +1000

    added gitignore and hardware basic script

---
+++ hardware/nodemcu-tester/nodemcu-tester.ino
@@ -0,0 +1,150 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <PubSubClient.h>
+
+#define LED D0
+#define red D5
+#define blue D6
+#define green D7
+#define triggerPin D4
+#define echoPin D3
+#define buzzer D8
+#define SERIAL_BAUD 9600
+
+const int parking_id = 1;
+String parking_status = "Available";
+
+const char* ssid = "OPTUS_A75988";
+const char* password = "tubergongs88107";
+const char* mqttServer = "192.168.0.10";
+const int mqttPort = 1883;
+const char* mqttUser = "";
+const char* mqttPassword = "";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+  WiFi.begin(ssid, password); // connect to Wifi
+  Serial.begin(SERIAL_BAUD); // setup serial communication
+  
+  pinMode(LED, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(buzzer, OUTPUT);
+
+  digitalWrite(triggerPin, LOW); // clean the trigger pin
+  digitalWrite(LED, HIGH); // turn off the Internal LED
+  noTone(buzzer);
+
+  // check if wifi not connected
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting..");
+  }
+
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+
+  // reconnect MQTT when connection lost  
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+ 
+    if (client.connect("security", mqttUser, mqttPassword )) {
+      Serial.println("connected");  
+    } else {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();  
+  }
+  
+  ledReactToDistance(getDistance());
+  delay(1000);
+  client.loop();
+}
+
+void ledReactToDistance(int distance) {
+  if (distance <= 3) {
+    colorLed(255, 0, 0);
+    tone(buzzer, 10);
+    delay(100);
+    noTone(buzzer);
+  } else {
+    colorLed(0, 255, 0);  
+  }
+}
+
+// trigger the ultrasonic sensor and get the distance value
+int getDistance() {
+  digitalWrite(triggerPin, LOW); // make sure the trigger is clean
+  delayMicroseconds(2);
+
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW); // turn off trigger after a duration
+
+  long duration = pulseIn(echoPin, HIGH); // read the duration value
+  int distance = duration * 0.034 / 2; // calculate the distance
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  return distance;
+}
+
+// blink the internal led every 1 second
+void blinkInternalLed() {
+  digitalWrite(LED, HIGH); // ESP Internal LED is reversed
+  Serial.println("HIGH");
+  delay(1000);
+  digitalWrite(LED, LOW); // ESP Internal LED is reversed
+  Serial.println("LOW");
+  delay(1000);    
+}
+
+// pass the rgb value to the function
+void colorLed(int r, int g, int b) {
+  analogWrite(red, r);
+  analogWrite(green, g);
+  analogWrite(blue, b);  
+}
+
+// reconnect method for MQTT
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("security")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+// mqtt callback method
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+ 
+  Serial.print("Message:");
+  String msg = "";
+  for (int i = 0; i < length; i++) {
+    msg = msg + ((char)payload[i]);
+  }
+  Serial.print(msg);
+
+  Serial.println();
+  Serial.println("-----------------------");
+}
