f9b26a7d2087bc70e987ca9b5489458bd6aa4148
unknown <rubymae.galila@gmail.com>
2020-05-20 11:38:11 +1000

    fix the wrong files that has been added

--- web/webApplication/app.py
+++ web/webApplication/app.py
@@ -1,20 +1,46 @@
-from flask import Flask, jsonify, render_template, Markup
+from flask import *
 import aws_controller
 from api import get_parking_info
 import boto3
 import json
 
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	#TODO: connect to dynamodb
-
+@app.route('/home')
+def home():
 	return render_template('book_slot.html')
 
-@app.route('/get-items')
-def get_items():
-	return jsonify(aws_controller.get_items())
+@app.route('/regInfo')
+def regInfo():
+	return render_template('booking.html')
+
+@app.route('/payment')
+def payment():
+	return render_template('payment.html')
+
+
+@app.route('/validateSlot', methods = ["POST"])  
+def validate():
+	message = ''
+	if request.method == 'POST':
+		return redirect(url_for("regInfo"))  
+	return redirect(url_for("home"))
+
+
+@app.route('/paySlot', methods = ["POST"])  
+def paySlot():  
+    if request.method == 'POST':  
+        return redirect(url_for("payment"))  
+    return redirect(url_for("regInfo"))  
+
+
+if __name__ == '__main__':
+	app.run()
+
+
+
 
 @app.route('/api')
 def api():
