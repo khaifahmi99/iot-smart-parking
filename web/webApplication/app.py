5082e2a70ca960f50972050923aed79122ed0576
khaifahmi99 <khaifahmi99@github.com>
2020-05-20 11:18:45 +1000

    added api script and edited app.py to serve root/api

--- web/webApplication/app.py
+++ web/webApplication/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify, render_template, Markup
 import aws_controller
+from api import get_parking_info
+import boto3
+import json
 
 app = Flask(__name__)
-
-
 
 @app.route('/')
 def index():
@@ -14,6 +15,11 @@ def index():
 @app.route('/get-items')
 def get_items():
 	return jsonify(aws_controller.get_items())
+
+@app.route('/api')
+def api():
+	json_obj = get_parking_info()
+	return json_obj
 
 if __name__ == '__main__':
 	app.run()
