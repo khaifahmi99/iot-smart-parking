ca4597a52388f2ee75b038bf7e0c33c8c921c74e
simonsala <simon1sala@hotmail.com>
2020-05-25 12:21:26 +1000

    Removed conflictive files in android app

--- android/parking_app/app/src/main/java/com/example/parking_app/interfaces/BaseSubject.java
+++
@@ -1,7 +0,0 @@
-package com.example.parking_app.interfaces;
-
-public interface BaseSubject {
-    public void registerObserver(Observer observer);
-    public void removeObserver(Observer observer);
-    public void notifyObservers();
-}
--- android/parking_app/app/src/main/java/com/example/parking_app/interfaces/Observer.java
+++
@@ -1,9 +0,0 @@
-package com.example.parking_app.interfaces;
-
-import com.amazonaws.mobileconnectors.dynamodbv2.document.datatype.Document;
-
-import java.util.List;
-
-public interface Observer {
-    void update(List<Document> docs);
-}
