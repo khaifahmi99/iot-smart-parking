28c2ad2af00dce1ae09c765caa157b0f8c2c5127
unknown <rubymae.galila@gmail.com>
2020-05-27 17:08:12 +1000

    fixed saving data to database

--- web/webApplication/aws_controller.py
+++ web/webApplication/aws_controller.py
@@ -1,5 +1,8 @@
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
+from random import randrange
+import datetime
+from datetime import datetime
 
 dynamodb = boto3.resource('dynamodb')
 
@@ -13,10 +16,7 @@ def get_spots_info():
 	return response['Items']
 
 def validate_slots(data):
-	#table = dynamodb.Table('parking_spot') 
-	#response = table.scan(
-	#	FilterExpression=Attr('parking_status').eq('Available') & Attr('parking_spot_id').IN("+Object.keys(data).toString()+ ")", ExpressionAttributeValues : titleObject
-    #)
+	
 	return data
 
 def validate_booking(data):
@@ -43,8 +43,42 @@ def validate_booking(data):
 	return data
 
 def save_booking(data):
-	rate = 10
-	ts = data.get('ts')
-	price = int(ts) * rate
+	rate = 2
+	payment_created = datetime.now()
+	ts_payment = payment_created.strftime("%Y-%m-%d %H:%M:%S")
+	book_created = datetime.now()
+	ts_booking = book_created.strftime("%Y-%m-%d %H:%M:%S")
+	payment_cost = payment_created.strftime("%H.%M")
+	book_cost = book_created.strftime("%H.%M")
+	pay_min = int(float(payment_cost)) * 60
+	book_min = (int((float(book_cost))) * 60) - 30
+	price = (pay_min - book_min) * rate
 	setattr(data, 'price', price)
+
+
+	name = data.get('full_name')
+	phonenum = data.get('phonenum')
+	plateNum = data.get('plateNum')
+	license = data.get('license')
+
+	
+
+	users_table = dynamodb.Table('users') 
+
+	users_table.put_item(
+		Item={
+			'user_id' : int(license),
+			'ts_payment' : ts_payment,
+			'user_carplate_number' : plateNum,
+			'user_fee' : price,
+			'user_hasMadePayment' : True,
+			'user_name' : name,
+			'user_phone' : phonenum
+		}
+	)
+
+	
+
+
+
 	return data
