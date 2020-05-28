5df148f9643572f85bed66010e185dca43ef04c4
unknown <rubymae.galila@gmail.com>
2020-05-28 14:13:05 +1000

    fixed booking time

--- web/webApplication/app.py
+++ web/webApplication/app.py
@@ -1,8 +1,10 @@
 from flask import *
 import aws_controller
+from api import *
 
 app = Flask(__name__)
 
+@app.route('/')
 @app.route('/home')
 def home():
 	spots = aws_controller.get_spots_info()
@@ -37,6 +39,22 @@ def validate_booking():
 			return render_template('booking.html', data = request.form, error = error)
 
 	return render_template('payment.html', message=message, data=validData)
+
+@app.route('/visualisation')
+def visualisation():
+	return render_template('visualisation.html')
+
+@app.route('/api/current')
+def api_current():
+	return get_parking_info()
+
+@app.route('/api/overview')
+def api_overview():
+	return get_overview_info()
+
+@app.route('/api/users')
+def api_users():
+	return get_user_info()
 
 if __name__ == '__main__':
 	app.run()
--- web/webApplication/aws_controller.py
+++ web/webApplication/aws_controller.py
@@ -1,11 +1,13 @@
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
+from random import randrange
+import datetime
+from datetime import datetime
 
-dynamodb = boto3.resource('dynamodb')
+dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2')
 
 
 def get_spots_info():
-
 	#call the table
 	table = dynamodb.Table('parking_spot') 
 	response = table.scan()
@@ -13,10 +15,7 @@ def get_spots_info():
 	return response['Items']
 
 def validate_slots(data):
-	#table = dynamodb.Table('parking_spot') 
-	#response = table.scan(
-	#	FilterExpression=Attr('parking_status').eq('Available') & Attr('parking_spot_id').IN("+Object.keys(data).toString()+ ")", ExpressionAttributeValues : titleObject
-    #)
+	
 	return data
 
 def validate_booking(data):
@@ -25,7 +24,7 @@ def validate_booking(data):
 	email = data.get('emailAdd')
 	license = data.get('license')
 	plateNum = data.get('plateNum')
-	ts = data.get('ts')
+	ts = data.get('time_booking')
 
 	if  name == '':
 		return 0
@@ -43,8 +42,35 @@ def validate_booking(data):
 	return data
 
 def save_booking(data):
-	rate = 10
-	ts = data.get('ts')
-	price = int(ts) * rate
+	rate = 7
+	name = data.get('full_name')
+	phonenum = data.get('phonenum')
+	plateNum = data.get('plateNum')
+	license = data.get('license')
+	time_booking = data.get('time_booking')
+
+	payment_created = datetime.now()
+	ts_payment = payment_created.strftime("%Y-%m-%d %H:%M:%S")
+	book_created = datetime.now()
+	ts_booking = book_created.strftime("%Y-%m-%d")
+	payment_time = payment_created.strftime("%H")
+	booking = time_booking.split(':')
+	pay_hours = int(float(payment_time))
+	book_hours = int(booking[0])
+	price = (pay_hours - book_hours) * rate
 	setattr(data, 'price', price)
+
+	users_table = dynamodb.Table('users') 
+
+	users_table.put_item(
+		Item={
+			'user_id' : int(license),
+			'ts_payment' : ts_payment,
+			'user_carplate_number' : plateNum,
+			'user_fee' : price,
+			'user_hasMadePayment' : True,
+			'user_name' : name,
+			'user_phone' : phonenum
+		}
+	)
 	return data
